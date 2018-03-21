5ca0b9a4c7d3f63c803cf0a2e7b955a29d8f1e90
Vladyslav Dzyhar <p9tnblwko@gmail.com>
2018-03-21 18:39:07 +0200

    gulpfile changed

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,20 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
  
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
- 
+
 gulp.task('watch', function () {
+	browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
   gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch("./*/*").on('change', browserSync.reload);
 });
